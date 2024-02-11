12e50946e5e34c638eeb88638698a0cfc5ecae39
Norman Huth <norman@huth.it>
2024-02-11 10:03:51 +0100

    fix: disable strict mysql for timestamp problem

--- app/Contracts/Models/CountryTrait.php
+++ app/Contracts/Models/CountryTrait.php
@@ -4,8 +4,8 @@ namespace App\Contracts\Models;
 
 use App\Models\Country;
 use App\Models\Recipe;
+use Arr;
 use Exception;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 trait CountryTrait
@@ -26,37 +26,37 @@ trait CountryTrait
 
         /* @var \NormanHuth\HellofreshScraper\Models\AbstractModel $hfModel */
         $columns = (new static())->getFillable();
-        $casts = (new static())->getCasts();
+        //$casts = (new static())->getCasts();
         $data = $hfModel->data();
 
-        foreach ($columns as $key => $column) {
-            $value = data_get($data, Str::camel(str_replace(array_keys($replace), array_values($replace), $column)));
-
-            if (data_get($casts, $column) == 'datetime') {
-                try {
-                    $value = Carbon::parse($value);
-                    $supported = Carbon::parse('1970-01-01 00:00:01');
-
-                    if ($value < $supported) {
-                        $value = $supported;
-                    }
-                } catch (Exception) {
-                    // silent
-                    $value = null;
-                }
-            }
-            unset($columns[$key]);
-            $columns[$column] = $value;
-        }
-
-        return $columns;
-        //return Arr::mapWithKeys(
-        //    $columns,
-        //    fn (string $column) => [$column => data_get(
-        //        $data,
-        //        Str::camel(str_replace(array_keys($replace), array_values($replace), $column))
-        //    )]
-        //);
+        //foreach ($columns as $key => $column) {
+        //    $value = data_get($data, Str::camel(str_replace(array_keys($replace), array_values($replace), $column)));
+        //
+        //    if (data_get($casts, $column) == 'datetime') {
+        //        try {
+        //            $value = Carbon::parse($value);
+        //            $supported = Carbon::parse('1970-01-01 00:00:01');
+        //
+        //            if ($value < $supported) {
+        //                $value = $supported;
+        //            }
+        //        } catch (Exception) {
+        //            // silent
+        //            $value = null;
+        //        }
+        //    }
+        //    unset($columns[$key]);
+        //    $columns[$column] = $value;
+        //}
+        //
+        //return $columns;
+        return Arr::mapWithKeys(
+            $columns,
+            fn (string $column) => [$column => data_get(
+                $data,
+                Str::camel(str_replace(array_keys($replace), array_values($replace), $column))
+            )]
+        );
     }
 
     protected function freshKey(string $column): string
