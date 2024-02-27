687dd5ff3118191c4e74a377f19864b850adda8e
Norman Huth <norman@huth.it>
2024-02-27 19:41:15 +0100

    perf: remove custom translatable method
    Fixed: https://github.com/spatie/laravel-translatable/releases/tag/6.6.1

--- app/Contracts/Models/AbstractTranslatableModel.php
+++ app/Contracts/Models/AbstractTranslatableModel.php
@@ -4,8 +4,6 @@ namespace App\Contracts\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use Spatie\Translatable\Events\TranslationHasBeenSetEvent;
 use Spatie\Translatable\HasTranslations;
 
 abstract class AbstractTranslatableModel extends Model
@@ -28,38 +26,5 @@ abstract class AbstractTranslatableModel extends Model
     protected function getCurrentTranslation(string $key): mixed
     {
         return $this->getTranslation($key, app()->getLocale());
-    }
-
-    /**
-     * @throws \Spatie\Translatable\Exceptions\AttributeIsNotTranslatable
-     */
-    public function setTranslation(string $key, string $locale, $value): self
-    {
-        $this->guardAgainstNonTranslatableAttribute($key);
-
-        $translations = $this->getTranslations($key);
-
-        $oldValue = $translations[$locale] ?? '';
-
-        if ($this->hasSetMutator($key)) {
-            $method = 'set' . Str::studly($key) . 'Attribute';
-
-            $this->{$method}($value, $locale);
-
-            $value = $this->attributes[$key];
-        } elseif ($this->hasAttributeSetMutator($key)) { // handle new attribute mutator
-            $this->setAttributeMarkedMutatedAttributeValue($key, $value);
-
-            $value = $this->attributes[$key];
-        }
-
-        $translations[$locale] = $value;
-
-        //$this->attributes[$key] = $this->asJson($translations);
-        $this->attributes[$key] = json_encode($translations, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
-        event(new TranslationHasBeenSetEvent($this, $key, $locale, $oldValue, $value));
-
-        return $this;
     }
 }
