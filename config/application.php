354fa1ade09678a20e4c23733287d04fb5290b08
Norman Huth <norman@huth.it>
2024-03-16 21:13:31 +0100

    fix: add missing configuration

---
+++ config/application.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Stupid file name, but I'm tired...
+ */
+
+return [
+    'filter' => [
+        'max_filterable_items' => 30,
+    ],
+    'pagination' => [
+        'per_page' => 12,
+    ],
+    'shopping_list' => [
+        'max_items' => 50,
+    ],
+];
