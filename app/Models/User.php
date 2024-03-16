5caa2f9a0724c4e16866e01c615c03f13635305d
Norman Huth <norman@huth.it>
2024-03-16 22:13:49 +0100

    feat: add model and migration for user recipe favorites

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -53,5 +54,14 @@ class User extends Authenticatable
     public function filters(): HasMany
     {
         return $this->hasMany(UserFilter::class);
+    }
+
+    /**
+     * The favorite recipes that belong to the user.
+     */
+    public function favorites(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class)
+            ->using(RecipeUser::class);
     }
 }
