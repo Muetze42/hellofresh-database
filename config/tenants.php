fecce74c5a69fadc88ddeddc67736f34e38eff30
Norman Huth <norman@huth.it>
2025-08-17 16:41:09 +0200

    feat: add tenants config

---
+++ config/tenants.php
@@ -0,0 +1,94 @@
+<?php
+
+return [
+    'at' => [
+        'locales' => ['de'],
+        'domain' => 'https://www.hellofresh.at',
+        'active' => true,
+    ],
+    'au' => [
+        'locales' => ['en'],
+        'domain' => 'https://www.hellofresh.com.au',
+        'active' => true,
+    ],
+    'be' => [
+        'locales' => ['nl', 'fr'],
+        'domain' => 'https://www.hellofresh.be',
+        'active' => true,
+    ],
+    'ca' => [
+        'locales' => ['en', 'fr'],
+        'domain' => 'https://www.hellofresh.ca',
+        'active' => true,
+    ],
+    'ch' => [
+        'locales' => ['de'],
+        'domain' => 'https://www.hellofresh.ch',
+        'active' => true,
+    ],
+    'de' => [
+        'locales' => ['de'],
+        'domain' => 'https://www.hellofresh.de',
+        'active' => true,
+    ],
+    'dk' => [
+        'locales' => ['da'],
+        'domain' => 'https://www.hellofresh.dk',
+        'active' => true,
+    ],
+    'fr' => [
+        'locales' => ['fr'],
+        'domain' => 'https://www.hellofresh.fr',
+        'active' => true,
+    ],
+    'gb' => [
+        'locales' => ['en'],
+        'domain' => 'https://www.hellofresh.co.uk',
+        'active' => true,
+    ],
+    'it' => [
+        'locales' => ['it'],
+        'domain' => 'https://www.hellofresh.it',
+        'active' => true,
+    ],
+    'lu' => [
+        'locales' => ['fr'],
+        'domain' => 'https://www.hellofresh.lu',
+        'active' => true,
+    ],
+    'nl' => [
+        'locales' => ['nl'],
+        'domain' => 'https://www.hellofresh.nl',
+        'active' => true,
+    ],
+    'no' => [
+        'locales' => ['nb'],
+        'domain' => 'https://www.hellofresh.no',
+        'active' => true,
+    ],
+    'nz' => [
+        'locales' => ['en'],
+        'domain' => 'https://www.hellofresh.co.nz',
+        'active' => true,
+    ],
+    'se' => [
+        'locales' => ['sv'],
+        'domain' => 'https://www.hellofresh.se',
+        'active' => true,
+    ],
+    'us' => [
+        'locales' => ['en'],
+        'domain' => 'https://www.hellofresh.com',
+        'active' => true,
+    ],
+    'ie' => [
+        'locales' => ['en'],
+        'domain' => 'https://www.hellofresh.ie',
+        'active' => true,
+    ],
+    'es' => [
+        'locales' => ['es'],
+        'domain' => 'https://www.hellofresh.es',
+        'active' => true,
+    ],
+];
